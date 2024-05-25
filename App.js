ce7fb5a2150680b49da2a0f2c915e64c7dcf3338
Nishant-ruhil <nishant9053ruhil@gmail.com>
2024-05-25 12:36:20 +0530

    Setup Stack Naviagator

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { OnBoardingScreen } from "./Screens/OnBoardingScreen";
+import AppNavigation from "./navigation/AppNavigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <AppNavigation />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Screens/OnBoardingScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export const OnBoardingScreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>Hello</Text>
+    </SafeAreaView>
+  )
+}
